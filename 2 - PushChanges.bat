6eccb9587e0666dda682332a880038e3d0a9d8ea
MadSabina7 <Mad.Sabina7@gmail.com>
2025-08-20 16:06:51 +0100

    P00250 - P00273

---
+++ 2 - PushChanges.bat
@@ -0,0 +1,5 @@
+@echo off
+python "D:\Other\Sabina London\Shopify Store\Planning\Templates\Products Preparation\_Products_\SabinaAssets\PushProducts.py"
+echo.
+echo Script finished. Press any key to close this window...
+pause
